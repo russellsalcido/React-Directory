c1641207bb1742c310dd186b95d9de081f78739e
Russell Salcido <russell.salcido@gmail.com>
2020-07-06 19:37:47 -0700

    Updates

---
+++ precache-manifest.4bd696dd9c44b7f34545f62f656b2cfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b63217c966f38641d22d99759c370c75",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "f3284ade13f6cc0097c1",
+    "url": "/react-gh-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b9b93b183d9c1e16f769",
+    "url": "/react-gh-pages/static/js/2.19f9d7d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-gh-pages/static/js/2.19f9d7d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3284ade13f6cc0097c1",
+    "url": "/react-gh-pages/static/js/main.2174af46.chunk.js"
+  },
+  {
+    "revision": "222f95b4d4642b24daa0",
+    "url": "/react-gh-pages/static/js/runtime-main.7ed2a0ab.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
